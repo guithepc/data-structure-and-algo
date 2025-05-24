2ffdbb9209d3cc7b00b97aa513a0bf8a1e933539
guithepc <guilhermepc@yahoo.com>
2025-05-24 10:56:42 -0300

    test

--- src/datasctructure/Main.java
+++ src/datasctructure/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
         System.out.println(newLinkedList.removeFirst().value);
         System.out.println(newLinkedList.removeFirst().value);
-        System.out.println(newLinkedList.removeFirst());
 
 
 
